74f7234d7097e2de11b3ac616a74bc55aed2ad77
Furior57 <Furior572@gmail.com>
2022-07-20 09:20:14 +0300

    20.07.22 Bean Live Cycle(Init,Destroy)

--- src/main/java/spring_introduction/animal/Cat.java
+++ src/main/java/spring_introduction/animal/Cat.java
@@ -5,6 +5,15 @@ public class Cat implements Pet{
     public Cat() {
         System.out.println("Cat bean is created");
     }
+
+    public void init() {
+        System.out.println("Class Cat: init method");
+    }
+
+    public void destroy() {
+        System.out.println("Class Cat: destroy method");
+    }
+
     @Override
     public void say() {
         System.out.println("Miau");
