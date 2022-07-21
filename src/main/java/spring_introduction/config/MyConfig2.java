fba7460ca92738e619d1a7fda513d6bf69d76397
Furior57 <Furior572@gmail.com>
2022-07-21 12:39:51 +0300

    21.07.22 Configuration container with java2

---
+++ src/main/java/spring_introduction/config/MyConfig2.java
@@ -0,0 +1,36 @@
+package spring_introduction.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
+import spring_introduction.animal.Cat;
+import spring_introduction.animal.PersonWithJava2;
+import spring_introduction.animal.Pet;
+
+@Configuration
+// этой записью мы указываем путь к файлу со значениями, задание значений в поля пишется так же как
+// и до этого
+@PropertySource("classpath:myApp.properties")
+public class MyConfig2 {
+    // Мы оставляем аннотацию @Configuration, но так как поиска в пакете никакого не будет
+    // нам нужно самим указать всю логику в этом классе.
+    // Для начала определим бин, для этого создадим метод возвращающий обьект Cat
+    // и пометим его аннотацией @Bean, id этого бина - название метода.
+    @Bean
+    @Scope("singleton")
+    public Pet catBean() {
+        return new Cat();
+    }
+
+    // Теперь посмотрим как внедрять зависимости
+    @Bean
+    public PersonWithJava2 personBean() {
+        // Конструктор этого класса требует обьект Pet, мы передаем в него метод возвращающий
+        // Cat, собственно все. Аннотация @Autowired в таком способе не используется, все
+        // зависимости мы прописываем вручную.
+        return new PersonWithJava2(catBean());
+    }
+
+
+}
